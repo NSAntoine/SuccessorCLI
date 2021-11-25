409142f6a9a8f044dd1b86c5b6d86c7e7627dc8f
Dabezt <osama.id78@gmail.com>
2021-11-25 14:00:33 +0300

    This should actually be here instead..

--- src/main.swift
+++ src/main.swift
@@ -151,13 +151,13 @@ if !DMGManager.DMGSinSCLIPathArray.isEmpty {
     }
 }
 
+print("Choose what to do below.")
 if !iPSWManager.iPSWSInSCLIPathArray.isEmpty {
     print("Found following iPSWs in \(SCLIInfo.shared.SuccessorCLIPath).")
     for i in 0...(iPSWManager.iPSWSInSCLIPathArray.count - 1) {
         print("[\(i)] Extract and use iPSW \(iPSWManager.iPSWSInSCLIPathArray[i])")
     }
 }
-print("Choose what to do below.")
 print("[\(iPSWManager.iPSWSInSCLIPathArray.count)] Download an iPSW")
 guard let input = readLine(), let inputInt = Int(input), inputInt <= iPSWManager.iPSWSInSCLIPathArray.count else {
     fatalError("Input must be a number and must be equal to or less than \(iPSWManager.iPSWSInSCLIPathArray.count)")
